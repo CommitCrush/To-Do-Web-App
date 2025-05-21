247ae086afe2c7b0e0c9fda354e03a86c592e115
Ese-tech <ese.osaie19@gmail.com>
2025-05-21 23:34:35 +0200

    Initialize Express server with CORS and logging middleware

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+const PORT = 3008;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.get("/api/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "pong" });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
